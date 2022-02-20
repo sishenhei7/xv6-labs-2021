52bfbcd3d2687a38ed090b78a9e5d6440091d79c
sishenhei7 <982505136@qq.com>
2022-02-20 23:47:14 +0800

    fix: fix pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,7 +5,7 @@
 int
 main(int argc, char *argv[]) {
   int parent_fd[2], child_fd[2];
-  int buf[1];
+  char parent_buf[1], child_buf[1];
 
   if(argc > 1){
     fprintf(2, "Usage: sleep ...\n");
@@ -16,21 +16,20 @@ main(int argc, char *argv[]) {
   pipe(child_fd);
 
   if (fork() == 0) {
-    close(parent_fd[0]);
-    close(child_fd[1]);
-    if (read(parent_fd[1], buf, 1)) {
-      fprintf(1, "%d: received ping\n", getpid());
-      write(child_fd[0], buf, 1);
-      close(child_fd[0]);
-    }
-  } else {
     close(parent_fd[1]);
     close(child_fd[0]);
-    write(parent_fd[0], "p", 1);
-    wait(0);
-    if (read(child_fd[1], buf, 1)) {
+    if (read(parent_fd[0], child_buf, 1) == 1) {
+      fprintf(1, "%d: received ping\n", getpid());
+      write(child_fd[1], "q", 1);
+      close(child_fd[1]);
+    }
+  } else {
+    close(parent_fd[0]);
+    close(child_fd[1]);
+    write(parent_fd[1], "p", 1);
+    if (read(child_fd[0], parent_buf, 1) == 1) {
       fprintf(1, "%d: received pong\n", getpid());
-      close(parent_fd[0]);
+      close(parent_fd[1]);
     }
   }
 
