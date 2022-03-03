4a0b27239868f1e6d7c65222e2b69d783c33cf7e
sishenhei7 <982505136@qq.com>
2022-03-03 09:26:37 +0800

    feat: add backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,15 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace(void) {
+  uint64 fp = r_fp();
+  uint64 max = PGROUNDUP(fp);
+
+  printf("backtrace:\n");
+  while (fp < max) {
+    printf("%p\n", *(uint64*)(fp-8));
+    fp = *(uint64*)(fp - 16);
+  }
+}
